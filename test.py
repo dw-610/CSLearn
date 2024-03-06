6e3396ef6fbb95181cf6d7bc8db97d1e39d9e7e9
Dylan W <139509928+dw-610@users.noreply.github.com>
2024-03-06 08:01:55 -0600

    added cifar10 training capability

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ def main():
     # set parameters
     lam = 1.0
     p = 1.0
+
+    # mnist
     M = tf.constant([
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         [1, 0, 1, 2, 3, 4, 5, 6, 7, 8],
@@ -21,15 +23,35 @@ def main():
         [7, 6, 5, 4, 3, 2, 1, 0, 1, 2],
         [8, 7, 6, 5, 4, 3, 2, 1, 0, 1],
         [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-    ], dtype=np.float32)
+    ], dtype=tf.float32)
+
+    # # cifar-10
+    # M = tf.constant([
+    #     [0, 2, 2, 3, 3, 3, 3, 3, 1, 2],
+    #     [2, 0, 3, 2, 2, 2, 2, 2, 2, 1],
+    #     [2, 3, 0, 1, 2, 1, 1, 2, 3, 3],
+    #     [3, 2, 1, 0, 2, 1, 1, 2, 3, 3],
+    #     [3, 2, 2, 2, 0, 2, 2, 1, 3, 3],
+    #     [3, 2, 1, 1, 2, 0, 1, 2, 3, 3],
+    #     [3, 2, 1, 1, 2, 1, 0, 2, 3, 3],
+    #     [3, 2, 2, 2, 1, 2, 2, 0, 3, 3],
+    #     [1, 2, 3, 3, 3, 3, 3, 3, 0, 2],
+    #     [2, 1, 3, 3, 3, 3, 3, 3, 2, 0],
+    # ], dtype=tf.float32)
+
+    if not wt.confirm_metric(M):
+        raise ValueError('The matrix M is not a metric.')
+
     M = M**p
     Kmat = tf.exp(-lam*M-1)
+
+    dataset = 'mnist'
 
     # breakpoint()
 
     # get the data and model
-    (x_trn, y_trn), (x_tst, y_tst) = wt.get_data()
-    encoder, model = wt.get_model()
+    (x_trn, y_trn), (x_tst, y_tst) = wt.get_data(dataset)
+    encoder, model = wt.get_model(dataset)
     encoder.summary()
     model.summary()
 
@@ -38,26 +60,25 @@ def main():
     model.compile(optimizer=optimizer)
 
     # train the model
-    for epoch in range(30):
-        print('\nEpoch: ', epoch+1, '/30')
-        for i in range(0, len(x_trn), 64):
-            x_batch = x_trn[i:i+32]
-            y_batch = y_trn[i:i+32]
-            pred = wt.train_step(model, optimizer, x_batch, y_batch, lam, Kmat)
-            if tf.reduce_any(tf.math.is_nan(pred)):
-                print('NaN prediction detected.')
-                break
-            print(f'\rBatch {i+1}/{len(x_trn)}', end='')
-        print()
-        print('Evaluating model...')
-        acc = wt.test_step(model, x_tst, y_tst)
-        acc = np.round(acc.numpy()*100, 2)
-        print(f'Accuracy: {acc:.2f}%')
+    wt.train_model(
+        epochs=20,
+        batch_size=128,
+        model=model,
+        optimizer=optimizer,
+        x_trn=x_trn,
+        y_trn=y_trn,
+        x_tst=x_tst,
+        y_tst=y_tst,
+        lam=lam,
+        Kmat=Kmat
+    )
 
     # evaluate the feature space
+    legend = [str(i) for i in range(10)] # MNIST
+    # legend = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
+    #           'dog', 'frog', 'horse', 'ship', 'truck'] # CIFAR-10
     features = encoder(x_tst, training=False).numpy()
-    plt.scatter(features[:,0], features[:,1], c=np.argmax(y_tst, axis=1))
-    plt.show()
+    wt.plot_features(features, y_tst, legend)
 
 if __name__ == '__main__':
     main()
