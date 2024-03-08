34b95a0e2b6a08052baf919f87a6be5ca4a26f3d
Dylan W <139509928+dw-610@users.noreply.github.com>
2024-03-08 09:46:24 -0600

    did some testing with recent changes

--- test_cslearn.py
+++ test_cslearn.py
@@ -33,35 +33,54 @@ def main():
             [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
         ]
     ).astype(np.float32)/9.0
-    BS = 32
-    LD = 2
+
     EPOCHS = 10
-    WP = 1.0
-    WL = 1.0
-    LOSS = 'categorical_crossentropy'
+    BATCH_SIZE = 64
+
+    LATENT_DIM = 2
+
+    LOSS = 'wasserstein'
+    WASS_P = 1.0
+    WASS_LAM = 1.0
+
+    ALPHA = 1.0
+    BETA = 0.0
+    WARM = 0.0
 
     # main code
-    ctrl = ImageLearningController('classifier', debug=False)
-    ctrl.create_data_loaders('mnist', batch_size=BS)
-    ctrl.create_learner(latent_dim=LD, output_activation='linear')
+    ctrl = ImageLearningController('domain_learner', debug=False)
+    ctrl.create_data_loaders('mnist', batch_size=BATCH_SIZE)
+    ctrl.create_learner(latent_dim=LATENT_DIM, output_activation='linear')
     ctrl.summarize_models()
     ctrl.compile_learner(
         loss=LOSS,
         metrics=['accuracy'],
         metric_matrix=M,
-        wasserstein_lam=WL,
-        wasserstein_p=WP,
-        schedule_type='cosine',
-        sch_init_lr=0.0001,
-        sch_warmup_steps=3*1876,
-        sch_warmup_target=0.001,
-        sch_decay_steps=7*1876,
+        alpha = ALPHA,
+        beta = BETA,
+        lam = 0.0,
+        wasserstein_lam=WASS_LAM,
+        wasserstein_p=WASS_P,
+        learning_rate=0.001,
+        # schedule_type='cosine',
+        # sch_init_lr=0.0001,
+        # sch_warmup_steps=3*1876,
+        # sch_warmup_target=0.001,
+        # sch_decay_steps=7*1876,
     )
-    ctrl.train_learner(epochs=EPOCHS)
+    legend = [str(i) for i in range(10)]
+    ctrl.train_learner(
+        epochs=EPOCHS, 
+        warmup=WARM
+    )
     ctrl.eval_plot_accuracy_curves()
-    ctrl.eval_plot_scattered_features(
-        legend = [str(i) for i in range(10)]
-    )
+    ctrl.eval_plot_scattered_features(legend=legend)
+    ctrl.eval_plot_scattered_protos(legend=legend)
+    ctrl.eval_plot_similarity_heatmap(legend=legend)
+    # ctrl.eval_visualize_all_dimensions()
+    ctrl.eval_compare_true_and_generated()
+
+    print(ctrl.prototypes)
 
     print('\nFinished running - no errors.\n')
 
